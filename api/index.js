85d4b2442fd9e9e2932d1efea337658292e517f5
prcuisa <33408705+prcuisa@users.noreply.github.com>
2025-11-19 13:06:28 +0000

    main

--- api/index.js
+++ api/index.js
@@ -1,18 +1,10 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const path = require("path");
-
-const app = express();
+const express=require("express");
+const serverless=require("serverless-http");
+const path=require("path");
+const app=express();
 app.use(express.json());
-
-// Import routes
-const notesRoutes = require("../server/routes/notes");
-
-// Main route
-app.use("/notes", notesRoutes);
-
-// For frontend pages
-app.use("/", express.static(path.join(__dirname, "../public")));
-
-module.exports = app;
-module.exports.handler = serverless(app);
+const notesRoutes=require("../server/routes/notes");
+app.use("/notes",notesRoutes);
+app.use("/",express.static(path.join(__dirname,"../public")));
+module.exports=app;
+module.exports.handler=serverless(app);
